02152675f84a67eb1bf6f630c4169feadfc8843d
DEADxMONARCH <vaibhav2005lucky@gmail.com>
2025-12-04 17:24:40 +0530

    level order traversing

---
+++ level_order_treversal.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+class node{
+    public:
+    int data ;
+    node* left;
+    node* right;
+    node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+class solution{
+    public:
+    vector<vector<int>> levelorder(node* root){
+        vector<vector<int>>ans;
+        if(root == NULL) return ans;
+        queue<node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level;
+            for(int i = 0; i < size; i++){
+                node* curr = q.front();
+                q.pop();
+                level.push_back(curr->data);
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+
+};
